ccaf43970ac4df4b602cbd2e092fc7aacdfe1c31
Yuki Yoshioka <yukiyoshiokap@gmail.com>
2016-07-20 01:24:47 +0900

    rebuilding site 2016年 7月20日 水曜日 01時24分47秒 JST

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,8 @@ git add -A
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
       then msg="$1"
-      fi
-      git commit -m "$msg"
-
-      # Push source and build repos.
-      git push origin master
-      git subtree push --prefix=public git@github.com:pyonk/blog.git gh-pages
+fi
+git commit -m "$msg"
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:pyonk/blog.git gh-pages
